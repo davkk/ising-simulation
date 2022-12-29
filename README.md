f0f4bca61f8ca8ec3d170c7e5a837462178d6d9a
Dawid Karp <dadulerk@gmail.com>
2022-12-29 14:03:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ising model simulation
+
+## Ideas:
+
+- Potts - dopuszczalnych jest wiecej stanow niz dwa
+- siec Erdos-Renyi
+- siec Barabasi-Albert
---
+++ src/Program.fs
@@ -0,0 +1,6 @@
+﻿// Define a new function to print a name.
+let printGreeting name =
+    printfn $"Hello {name} from F#!"
+
+// Call your new function!
+printGreeting "Ana"
